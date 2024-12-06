b24af04ba77ec9085dae682c91f2227a7d526ae2
tiaraaulia1008 <tiaraaulia1008@gmail.com>
2024-12-06 22:32:28 +0700

    Update Cell.java

--- sudoku/Cell.java
+++ sudoku/Cell.java
@@ -8,24 +8,27 @@
 * 3 - 5026231148 - Tiara Aulia Azadirachta Indica
 */
 
+package sudoku;
+
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.JTextField;
+
 /**
- * The Cell class model the cells of the Sudoku puzzle, by customizing (subclass)
- * the javax.swing.JTextField to include row/column, puzzle number and status.
+ * The Cell class models the cells of the Sudoku puzzle, by customizing (subclass)
+ * the javax.swing.JTextField to include row/column, puzzle number, and status.
  */
 public class Cell extends JTextField {
     private static final long serialVersionUID = 1L;  // to prevent serial warning
 
     // Define named constants for JTextField's colors and fonts
-    //  to be chosen based on CellStatus
+    // to be chosen based on CellStatus
     public static final Color BG_GIVEN = new Color(240, 240, 240); // RGB
     public static final Color FG_GIVEN = Color.BLACK;
     public static final Color FG_NOT_GIVEN = Color.GRAY;
-    public static final Color BG_TO_GUESS  = Color.YELLOW;
+    public static final Color BG_TO_GUESS = Color.YELLOW;
     public static final Color BG_CORRECT_GUESS = new Color(0, 216, 0);
-    public static final Color BG_WRONG_GUESS   = new Color(216, 0, 0);
+    public static final Color BG_WRONG_GUESS = new Color(216, 0, 0);
     public static final Font FONT_NUMBERS = new Font("OCR A Extended", Font.PLAIN, 28);
 
     // Define properties (package-visible)
@@ -35,7 +38,6 @@ public class Cell extends JTextField {
     int number;
     /** The status of this cell defined in enum CellStatus */
     CellStatus status;
-    boolean isHighlighted = false;  // Flag to highlight matching cells
 
     /** Constructor */
     public Cell(int row, int col) {
@@ -56,32 +58,44 @@ public class Cell extends JTextField {
 
     /** This Cell (JTextField) paints itself based on its status */
     public void paint() {
+        Color bgColor = BG_GIVEN;  // Default background color
+        Color fgColor = FG_GIVEN;  // Default foreground color
+
         if (status == CellStatus.GIVEN) {
-            // Inherited from JTextField: Set display properties
             super.setText(number + "");
             super.setEditable(false);
-            super.setBackground(BG_GIVEN);
-            super.setForeground(FG_GIVEN);
         } else if (status == CellStatus.TO_GUESS) {
-            // Inherited from JTextField: Set display properties
             super.setText("");
             super.setEditable(true);
-            super.setBackground(BG_TO_GUESS);
-            super.setForeground(FG_NOT_GIVEN);
-        } else if (status == CellStatus.CORRECT_GUESS) {  // from TO_GUESS
-            super.setBackground(BG_CORRECT_GUESS);
-        } else if (status == CellStatus.WRONG_GUESS) {    // from TO_GUESS
-            super.setBackground(BG_WRONG_GUESS);
+            bgColor = BG_TO_GUESS;
+            fgColor = FG_NOT_GIVEN;
+        } else if (status == CellStatus.CORRECT_GUESS) {
+            bgColor = BG_CORRECT_GUESS;
+        } else if (status == CellStatus.WRONG_GUESS) {
+            bgColor = BG_WRONG_GUESS;
         }
 
-        // Tambahkan efek highlight di atas warna default
-        if (isHighlighted) {
-            super.setBackground(new Color(173, 216, 230)); // Highlight overlay dengan warna biru muda
-        }
+        super.setBackground(bgColor);
+        super.setForeground(fgColor);
     }
 
-    // Method to highlight the cell when value matches
-    public void setHighlighted(boolean highlighted) {
-        this.isHighlighted = highlighted;
+
+    public void setTheme(Color bgGiven, Color fgGiven, Color bgToGuess, Color fgNotGiven) {
+        if (status == CellStatus.GIVEN) {
+            super.setBackground(bgGiven);
+            super.setForeground(fgGiven);
+        } else if (status == CellStatus.TO_GUESS) {
+            super.setBackground(bgToGuess);
+            super.setForeground(fgNotGiven);
+        }
     }
+        public void applyTheme(Color bgGiven, Color fgGiven, Color bgToGuess, Color fgNotGiven) {
+            if (this.isEditable()) {
+                setBackground(bgToGuess);
+                setForeground(fgNotGiven);
+            } else {
+                setBackground(bgGiven);
+                setForeground(fgGiven);
+            }
+        }
 }
