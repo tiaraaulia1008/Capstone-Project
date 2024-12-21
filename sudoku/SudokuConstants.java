3bc6c59f3a0ac071f8d1db68d76845959d17c66f
tiaraaulia1008 <tiaraaulia1008@gmail.com>
2024-12-21 19:38:03 +0700

    Penambahan code dasar sudoku

---
+++ sudoku/SudokuConstants.java
@@ -0,0 +1,22 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #8
+ * 1 - 5026231023 - Nadya Luthfiyah Rahma
+ * 2 - 5026231094 - Davina Almeira
+ * 3 - 5026231148 - Tiara Aulia Azadirachta Indica
+ */
+
+//To store the named constants such as GRID_SIZE - to be referred to as SudokuConstants.GRID_SIZE.
+/**
+ * Define the named constants used in many classes.
+ */
+package sudoku;
+
+public class SudokuConstants {
+    /** Size of the board */
+    public static final int GRID_SIZE = 9;
+    /** Size of sub-grid of the board */
+    public static final int SUBGRID_SIZE = 3;
+}
